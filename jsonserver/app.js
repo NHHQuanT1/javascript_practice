af392c5222bf0db46b61e81a0d95892e7af0c641
HongQuan <hq7t1k58@gmail.com>
2024-10-14 23:22:44 +0700

    update 14/10 by Quan

---
+++ jsonserver/app.js
@@ -0,0 +1,9 @@
+var courseAPI = 'http://localhost:3000/courses'
+
+fetch(courseAPI)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(courses) {
+        console.log(courses);
+    });
